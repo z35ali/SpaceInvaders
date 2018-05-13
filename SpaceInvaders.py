0f27479dfd46dd67021eac55bde22bb2160f4fdb
Zafar Ali <zafar.a.ali2@gmail.com>
2018-05-13 11:59:02 -0400

    Add Images

--- SpaceInvaders.py
+++ SpaceInvaders.py
@@ -13,6 +13,12 @@ turtle.tracer(1)
 wn=turtle.Screen()
 wn.bgcolor("black")
 wn.title("Space Invaders")
+wn.bgpic("space_invaders_background.gif")
+
+#Register the shapes
+turtle.register_shape("invader.gif")
+turtle.register_shape("player.gif")
+
 
 #Draw border
 border_pen=turtle.Turtle()
@@ -43,7 +49,7 @@ score_pen.hideturtle()
 #Create the player turtle
 player=turtle.Turtle()
 player.color("blue")
-player.shape("triangle")
+player.shape("player.gif")
 player.penup()
 player.speed(0)
 player.setposition(0, -250)
@@ -64,7 +70,7 @@ for i in range(number_of_enemies):
 
 for enemy in enemies:
     enemy.color("red")
-    enemy.shape("circle")
+    enemy.shape("invader.gif")
     enemy.penup()
     enemy.speed(0)
     x=random.randint(-200,200)
